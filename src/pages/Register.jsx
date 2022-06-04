91761ed812adb79f138c1d4209b20d543616914b
hakamfaza <hakamfaza@gmail.com>
2022-06-04 12:20:39 +0700

    add auth register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,20 +1,59 @@
-import React from 'react';
-import Card from '../components/card/Card';
+import React, { useState } from 'react';
 import '../styles/auth.css';
+import { IoIosArrowBack } from 'react-icons/io';
+import { AiFillEyeInvisible, AiFillEye, AiOutlineGoogle } from 'react-icons/ai';
 
 export default function Register() {
+  const [visibel, setVisible] = useState(false);
   return (
     <div className="auth">
-      <div className="bg-success">Hallo</div>
       <div className="container">
-        <div className="row">
-          <div className="col">1 of 2</div>
-          <div className="col">2 of 2</div>
-        </div>
-        <div className="row">
-          <div className="col">1 of 3</div>
-          <div className="col">2 of 3</div>
-          <div className="col">3 of 3</div>
+        <div className="card shadow">
+          <div className="header">
+            <IoIosArrowBack className="iconBack" />
+            <p className="title">Register</p>
+          </div>
+          <p className="text">Let’s create your account!</p>
+          <form action="">
+            <div className="form">
+              <label className="label" htmlFor="username">
+                Name
+              </label>
+              <input type="text" className="input" id="username" placeholder="Enter your name" />
+            </div>
+            <div className="form">
+              <label className="label" htmlFor="email">
+                Email
+              </label>
+              <input type="email" className="input" id="email" placeholder="Enter your email" />
+            </div>
+            <div className="form">
+              <label className="label" htmlFor="password">
+                Name
+              </label>
+              <input
+                type={visibel ? 'text' : 'password'}
+                className="input"
+                id="password"
+                placeholder="Enter your password"
+              />
+              {visibel ? (
+                <AiFillEye className="isShow" onClick={() => setVisible(false)} />
+              ) : (
+                <AiFillEyeInvisible className="isShow" onClick={() => setVisible(true)} />
+              )}
+            </div>
+          </form>
+          <button className="btn">Register</button>
+          <div className="with">
+            <hr className="hr" />
+            <p>Register with</p>
+            <hr className="hr" />
+          </div>
+          <button className="btnGoogle">
+            <AiOutlineGoogle className="iconGoogle" />
+            Google
+          </button>
         </div>
       </div>
     </div>
