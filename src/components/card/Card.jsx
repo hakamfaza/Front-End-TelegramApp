8a1f5f112d69d5ad937ce10b5713e867dff23134
hakamfaza <hakamfaza@gmail.com>
2022-06-04 22:00:20 +0700

    add components card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import user from '../../assets/users.png';
 
 export default function Card() {
   return (
-    <div>
-      <p>Login</p>
-      <p>Lo</p>
+    <div className="flex p-3">
+      <img src={user} alt="user" className="w-16 h-16 rounded-xl ml-3" />
+      <div className="p-2 ml-2">
+        <p className="text-dark-color text-base font-medium max-w-sm">Thersa Weeb</p>
+        <p className="text-secondary text-sm">Lorem ipsum dolor sit</p>
+      </div>
+      <div className="ml-4">
+        <p className="text-grey-color text-sm mt-3">15:00</p>
+        <p className="bg-secondary text-primary w-auto h-5 text-center text-sm rounded-full">1</p>
+      </div>
     </div>
   );
 }
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import Card from '../components/card/Card';
 
-export default function Home() {
-  return <div className="text-secondary">hallo</div>;
+export default function Chat() {
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      <div>
+        <Card />
+      </div>
+      <div className="bg-secondary col-span-3">3</div>
+    </div>
+  );
 }
